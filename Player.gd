b927fa6727b2026da6a674b15c659212be1b8c25
TiredFox <86696742+TiredFox@users.noreply.github.com>
2021-07-03 23:58:19 -0500

    Fixing your corrupted ass world file

--- Player.gd
+++ Player.gd
@@ -1,20 +1,3 @@
 extends KinematicBody2D
 
 var tile_size = 16
-#var inputs = {"ui_right": Vector2.RIGHT,
-#"ui_left": Vector2.LEFT,
-#"ui_up": Vector2.UP,
-#"ui_down": Vector2.DOWN}
-
-func _ready():
-	position = position.snapped(Vector2.ONE * tile_size)
-	position += Vector2.ONE * tile_size / 2
-
-
-func _physics_process(delta):
-	var move_dir = 0
-	if Input.is_action_pressed("ui_right"):
-		move_dir += 1
-	if Input.is_action_pressed("ui_left"):
-		move_dir -= 1
-	move_and_slide(Vector2(move_dir * tile_size))
